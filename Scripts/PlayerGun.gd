ffb4443f75697afaef23a716b762a3da40b8b3fe
Johonel <jle14@huskers.unl.edu>
2022-03-06 21:16:55 -0600

    BulletPt2
    Bullets shoot out of gun now

--- Scripts/PlayerGun.gd
+++ Scripts/PlayerGun.gd
@@ -1,24 +1,12 @@
 extends Spatial
 
-export var bullet : PackedScene 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+export var bullet_scene : PackedScene 
 
 func _unhandled_input(event):
 	if event.is_action_pressed("shoot"):
 		shoot()
 
 func shoot ():
-	var bulletThing = bullet.instance()
-	bulletThing.transform = transform
-	get_tree().get_root().add_child(bulletThing)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	var bullet : Spatial = bullet_scene.instance()
+	bullet.transform = global_transform
+	get_tree().get_root().add_child(bullet)
