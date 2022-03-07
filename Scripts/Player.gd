7ef5e483224bdc16e17482b8da815799af2e6611
swoods45 <57550537+Theportalmasters@users.noreply.github.com>
2022-03-06 20:00:50 -0600

    Smooth Movement

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,13 +1,11 @@
 extends CharacterController
 
-func _unhandled_input(event):
-	var action = event.is_action("ui_down") or event.is_action("ui_up")
-	action = action or event.is_action("ui_left") or event.is_action("ui_right")
-	if action:
-		var x = Input.get_axis("ui_left","ui_right")
-		var y = Input.get_axis("ui_up","ui_down")
-		var realitive_movement = Vector2(x,y)
-		request_movement(realitive_movement)
+func _physics_process(delta):
+	var x = Input.get_axis("ui_left","ui_right")
+	var y = Input.get_axis("ui_up","ui_down")
+	var realitive_movement = Vector2(x,y)
+	request_movement(realitive_movement)
+	
 
 
 func _on_Camera_rotation_requested(amount):
