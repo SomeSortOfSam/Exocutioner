21a61e410119fbd6f50f5fcc510902267c9d5e24
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-03-16 16:29:37 -0500

    BareBones Enemy AI
    Reorganized as well

---
+++ Character/Enemy/Enemy.gd
@@ -0,0 +1,20 @@
+extends CharacterController
+
+var target : Spatial
+
+func _process(_delta):
+	if target:
+		var position = target.to_global(target.translation)
+		position = to_local(position)
+		position = Vector2(position.x,position.z)
+		var my_position = Vector2(translation.x,translation.z)
+		request_movement(position - my_position)
+
+
+func _on_Area_area_entered(area):
+	target = area
+
+
+func _on_Area_area_exited(area):
+	if area == target:
+		target = null
