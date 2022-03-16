c0af2ba6de430e5c4af160ebd6c156681ab4c1c9
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-03-16 17:08:22 -0500

    Test map?
    The test failed

--- Character/Player/Player.gd
+++ Character/Player/Player.gd
@@ -5,8 +5,6 @@ func _physics_process(_delta):
 	var y = Input.get_axis("ui_up","ui_down")
 	var realitive_movement = Vector2(x,y)
 	request_movement(realitive_movement)
-	
-
 
 func _on_Camera_rotation_requested(amount):
 	rotate(transform.basis.y, amount)
