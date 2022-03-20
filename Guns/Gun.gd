0b9fb89780c5767f32a14eeda004d756ef9cf3d3
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-03-20 17:40:07 -0500

    Automatic Gun

--- Guns/Gun.gd
+++ Guns/Gun.gd
@@ -2,13 +2,28 @@ extends Spatial
 class_name Gun
 
 onready var sounds : AudioStreamPlayer3D = $GunSounds
+onready var ray : RayCast = $RayCast
+onready var shot_in_betweens = $ShotBetweens
 
 export(Resource) var gun_kind
 
 var shooting := false
 
+func _ready():
+	equip_gun_kind()
+
+func equip_gun_kind():
+	if gun_kind:
+		if !gun_kind.bullet:
+			ray.cast_to = ray.cast_to.normalized() * gun_kind.target_range
+
+func stop_shooting():
+	sounds.stop()
+	shooting = false
+
 func shoot() -> bool:
-	if gun_kind and !shooting:
+	var condition = gun_kind and (gun_kind.automatic or !sounds.playing)
+	if condition:
 		if gun_kind.bullet:
 			var bullet : Spatial = gun_kind.bullet.instance()
 			bullet.transform = global_transform
@@ -16,11 +31,12 @@ func shoot() -> bool:
 		else:
 			print('histscan')
 		sounds.stream = gun_kind.get_shot_sound()
-		if sounds.stream:
+		if sounds.stream and !sounds.playing:
 			sounds.play()
-			shooting = true
-		return true
-	return false
+			shot_in_betweens.start(sounds.stream.get_length())
+		shooting = gun_kind.automatic
+	return condition
 
-func _on_GunSounds_finished():
-	shooting = false
+func _on_ShotBetweens_timeout():
+	if shooting:
+		shoot()
--- Guns/PlayerGun.gd
+++ Guns/PlayerGun.gd
@@ -1,24 +1,30 @@
 extends Gun
 
 onready var sprite : AnimatedSprite = $CanvasLayer/Control/Sprite
+onready var cursor : TextureRect = $CanvasLayer/Cursor
 
 func _ready():
 	if gun_kind:
 		sprite.frames = gun_kind.animations
 		sprite.play('Idle')
+		cursor.texture = gun_kind.cursor
 
 func _unhandled_input(event):
 	if event.is_action_pressed("shoot"):
-		var _moot = shoot()
+		shoot()
+	if event.is_action_released("shoot") and gun_kind.automatic:
+		stop_shooting()
 
-func shoot() -> bool:
-	if .shoot() and sprite.frames:
-		sprite.play(gun_kind.get_shot_name())
-		sprite.frame = 0
-		return true
-	return false
+func stop_shooting():
+	.stop_shooting()
+	sprite.play("EndShooting")
 
+func shoot():
+	if .shoot():
+		sprite.play("Shooting" if gun_kind.automatic else "Shoot")
+		if !gun_kind.automatic:
+			sprite.frame = 0
 
 func _on_Sprite_animation_finished():
-	if sprite.frames and sprite.animation != 'Idle':
+	if sprite.frames and sprite.animation in ["Shoot", "EndShooting"]:
 		sprite.play('Idle')
