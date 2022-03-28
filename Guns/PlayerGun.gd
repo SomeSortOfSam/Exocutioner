2694d041f5e24c5cf998e96098a985032808a276
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-03-27 19:44:35 -0500

    added weapon switching for demo

--- Guns/PlayerGun.gd
+++ Guns/PlayerGun.gd
@@ -3,13 +3,25 @@ extends Gun
 onready var sprite : AnimatedSprite = $CanvasLayer/Control/Sprite
 onready var cursor : TextureRect = $CanvasLayer/Cursor
 
+export(Array,Resource) var guns
+
+var cur_gun = 0
+
 func _ready():
-	if gun_kind:
+	equip_gun_kind()
+
+func equip_gun_kind():
+	.equip_gun_kind()
+	if gun_kind and sprite:
 		sprite.frames = gun_kind.animations
 		sprite.play('Idle')
 		cursor.texture = gun_kind.cursor
 
 func _unhandled_input(event):
+	if event.is_action_pressed("ui_page_down"):
+		cur_gun = (cur_gun + 1) % guns.size()
+		gun_kind = guns[cur_gun]
+		equip_gun_kind()
 	if event.is_action_pressed("shoot"):
 		shoot()
 	if event.is_action_released("shoot") and gun_kind.automatic:
