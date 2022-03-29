212cc96ba85f7517e7db8d0787981623579d08a3
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-03-28 22:57:53 -0500

    Implemented CLAW

---
+++ Guns/CLAW/Claw.gd
@@ -0,0 +1,14 @@
+extends Gun
+
+onready var ray : RayCast = $RayCast
+
+func _shoot():
+	if ray.is_colliding() and not sprite.animation == 'Grab':
+		sprite.play('Grab')
+		sounds.play()
+		var hit : Node = ray.get_collider().get_parent()
+		if hit.has_method("_on_HitBox_area_entered"):
+			hit._on_HitBox_area_entered(ray)
+
+func _on_Sprite_animation_finished():
+	sprite.play('Idle')
