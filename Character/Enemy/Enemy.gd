c6949eff14d78f0991bbcfc37f6eb9096ff4d313
SomeSortOfSam <57550537+Theportalmasters@users.noreply.github.com>
2022-03-30 13:11:57 -0500

    Enemy AI Fixed
    Forgot how local space worked for a bit there

--- Character/Enemy/Enemy.gd
+++ Character/Enemy/Enemy.gd
@@ -1,19 +1,19 @@
 extends CharacterController
 
+export var target_player_distance : float = 1
+
 var target : Spatial
 
 func _process(_delta):
 	if target:
 		var position = target.to_global(target.translation)
 		position = to_local(position)
-		position = Vector2(position.x,position.z)
-		var my_position = Vector2(translation.x,translation.z)
-		request_movement(position - my_position)
+		if position.distance_squared_to(Vector3.ZERO) > pow(target_player_distance,2):
+			request_movement(Vector2(position.x,position.z).normalized() * speed)
 
 
 func _on_Area_area_entered(area):
 	target = area
-
 
 func _on_Area_area_exited(area):
 	if area == target:
